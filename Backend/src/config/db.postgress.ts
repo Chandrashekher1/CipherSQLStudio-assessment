452d22ad2be327c3a6db4cfb7eddd24746e49819
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-12-10 17:41:23 +0530

    feat: change local connection to client connection

--- Backend/src/config/db.postgress.ts
+++ Backend/src/config/db.postgress.ts
@@ -1,13 +1,22 @@
-import {Pool} from "pg"
+import { Client } from "pg"
+import dotenv from "dotenv"
+dotenv.config()
 
-
-export const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'cipherSQLstudio',
-    password: 'postgres',
-    port: 5432,
+export const client = new Client({
+   connectionString: process.env.POSTGRES_URL
 })
 
+export const postgressDB = async function connect(){
+    try{
+        await client.connect()
+        console.log("PostgresDB connected")
+    }
+    catch(error){
+        console.log("PostgresDB connection error", error)
+    }
+}
 
 
+export const setWorkSpaceSchema = async  (workspaceId : string) => {
+    await client.query(`SET search_path TO workspace_${workspaceId}`)
+}
