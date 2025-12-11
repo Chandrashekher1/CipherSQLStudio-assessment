8b0c247b8fce95101130097a5bd5e9ea87e931a3
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-12-11 23:43:54 +0530

    feat: add result api call and pass as props

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,17 +1,43 @@
 import { Header } from "@/components/Header";
 import Result from "@/components/Result";
-import { Sidebar } from "@/components/sidebar/Sidebar";
+import Sidebar from "@/components/sidebar/Sidebar";
 import { SqlEditor } from "@/components/SqlEditor";
+import { runQuery } from "@/lib/api";
+import { useState } from "react";
+import { toast } from "sonner";
 
 export default function Dashboard() {
+    const [results, setResults] = useState<any[]>([]);
+    const [loading, setLoading] = useState(false);
+
+    const handleRunQuery = async (query: string) => {
+        setLoading(true);
+        try {
+            const WORKSPACE_ID = "1765384183209"; 
+            const data = await runQuery(WORKSPACE_ID, query);
+            if (data.error) {
+                toast.error(data.message);
+                setResults([]);
+            } else {
+                setResults(data?.rows || []);
+            }
+        } catch (err: any) {
+            toast.error(err.message || "Failed to run query");
+            setResults([]);
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
         <div>
             <Header />
-            <Sidebar/>
-
-            <div className="mx-20 my-4">
-                <SqlEditor/>
-                <Result/>
+            <div className="flex">
+                <Sidebar/>
+                <div className="mx-5 my-4 w-full h-[calc(100vh-100px)] overflow-hidden flex flex-col">
+                    <SqlEditor onRunQuery={handleRunQuery} isLoading={loading} />
+                    <Result data={results} loading={loading} />
+                </div>
             </div>
         </div>
     );
