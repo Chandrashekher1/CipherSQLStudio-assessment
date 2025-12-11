b48fd663ca1b9cb40c67e3112323b3610f436f3a
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-12-11 23:42:21 +0530

    feat: add toast component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
+import { Toaster } from 'sonner'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
+       <Toaster />
     </div>
   )
 }
