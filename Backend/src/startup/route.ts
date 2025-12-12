57f839dde8d0a48c400d81060b816b3223d5f026
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-12-12 19:31:31 +0530

    feat: add route of llm

--- Backend/src/startup/route.ts
+++ Backend/src/startup/route.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import workspaceRoute from "../routes/workspace.route";
 import sqlRoute from "../routes/sql.route";
+import llmRoute from "../routes/llm.routes";
 
 export const startup = (app : express.Application) => {   
     app.use(express.json())
     app.use(express.urlencoded({extended: true}))
-    app.use('/create', workspaceRoute)
+    app.use('/workspace', workspaceRoute)
     app.use('/sql', sqlRoute)
+    app.use('/llm', llmRoute)
 }
