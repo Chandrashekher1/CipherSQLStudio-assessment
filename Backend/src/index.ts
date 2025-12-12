efc4f940a574398cf9a6f9f460cc13e62b26ec46
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-12-12 19:30:42 +0530

    feat: add allow cors

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,12 +2,19 @@ import express from "express";
 import {startup} from "./startup/route";
 import { mongoDB } from "./config/db.mongo";
 import { postgressDB } from "./config/db.postgress";
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors({
+    origin: "*"
+}))
+
 startup(app)    
+
 postgressDB()
 mongoDB()
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
